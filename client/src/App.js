d0cbc0f32bf00edb0ccdf53d602a89e6392468e6
rodeloescueta <cxcxcxtest@gmail.com>
2020-12-25 21:44:10 +0800

    add material ui and familiarizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
+import Button from '@material-ui/core/Button'
+
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 function App() {
   return (
     <div className="App">
+      <ButtonGroup variant="contained" >
+        <Button startIcon={<SaveIcon />} color="primary">
+          Save
+        </Button>
+        <Button startIcon={<DeleteIcon />} color="secondary">
+          Discard
+        </Button>
+      </ButtonGroup>
+      
       <h1>Todo App</h1>
       <p>Display the list of to do tasks here with basic CRUD operations</p>
     </div>
